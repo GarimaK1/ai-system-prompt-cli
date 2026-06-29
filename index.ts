78f68c9fe0c3e686ec17901e62a0658f23174efb
Garima Khandekar <garima.k.03@gmail.com>
2026-06-29 14:22:23 +0530

    added loading spinner use, cleaned CLI UI

--- index.ts
+++ index.ts
@@ -1,84 +1,16 @@
-import { input, select, confirm, Separator } from "@inquirer/prompts";
 import { styleText } from "node:util";
-import validateInput from "./validatorFunction.ts";
-import {
-  SUMMARIZE,
-  EXPLAIN,
-  GENERATE_TESTS,
-  CHANGE_TONE,
-  EXIT,
-} from "./myConstants.ts";
 import { callAIModel } from "./callAIModel.ts";
+import { EXIT } from "./myConstants.ts";
+import {
+  getConfirmation,
+  inputPrompt,
+  optionSelection,
+} from "./inquirerQuestions.ts";
+import spinner from "./spinner.ts";
 
 // const successMessage = styleText("green", "Success!");
 // console.log(successMessage);
-
 // console.log(styleText(["underline", "italic"], "My italic underlined message"));
-
-// Function to display the system prompt options
-async function optionSelection() {
-  const answer = await select({
-    message: "Select a menu option",
-    choices: [
-      {
-        name: "1. Summarize text",
-        value: SUMMARIZE,
-        short: "Summarize text",
-        description: "Generates a concise summary of the provided text.",
-      },
-      {
-        name: "2. Explain code",
-        value: EXPLAIN,
-        short: "Explain code",
-        description: "Provides a detailed explanation of the provided code.",
-      },
-      // new Separator(),
-      {
-        name: "3. Generate test cases for a function",
-        value: GENERATE_TESTS,
-        short: "Generate test-cases",
-        description: "Creates test-cases for the provided function.",
-      },
-      {
-        name: "4. Rewrite the provided text in a different tone",
-        value: CHANGE_TONE,
-        short: "Change tone",
-        description: "Change the tone and rewrite the provided text.",
-      },
-      {
-        name: "5. Exit CLI",
-        value: EXIT,
-        short: "Exit CLI",
-        description: "Exit the CLI.",
-      },
-    ],
-  });
-
-  return answer;
-}
-
-// Getting input for user prompt
-async function inputPrompt() {
-  const inputAnswer = await input({
-    message: `Enter user prompt.`,
-    required: true,
-    validate: (value) =>
-      validateInput(value) ||
-      "Input contains potentially unsafe content. Update input and try again.",
-    // theme: {
-    //   validationFailureMode: "clear",
-    // },
-  });
-  return inputAnswer;
-}
-
-// Confirmation question
-async function getConfirmation() {
-  const answer = await confirm({
-    message: "Do you want to continue?",
-  });
-  return answer;
-}
 
 try {
   console.log(
@@ -106,11 +38,16 @@ try {
     }
 
     const userPrompt = await inputPrompt();
-    console.log(`Your answer: ${userPrompt}`);
+    // console.log(`Your answer: ${userPrompt}`);
 
     // Make an API call to OpenAI with the selected option and user prompt
+    // Add a "Loading answer..." message to indicate that the API call is in progress
+    // Usage
+    const mySpinner = spinner("Fetching response from AI model...");
     const response = await callAIModel(selectAnswer, userPrompt);
-    console.log("Output Text:\n", response.output_text, "\n");
+    mySpinner.stop("✓ Done!");
+
+    console.log("\nOutput Text:\n", response.output_text, "\n");
     console.log("Model Used:\n", response.model, "\n");
     console.log("Status:\n", response.status, "\n");
     console.log("Usage:\n", response.usage, "\n");
